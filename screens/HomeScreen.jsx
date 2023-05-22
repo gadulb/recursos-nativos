0d94b8e6a0b61e36d92be5efe1115fca0ff9054e
gadulb <gabrieli.lembeck@alunos.sc.senac.br>
2023-05-22 15:30:56 -0300

    add

--- screens/HomeScreen.jsx
+++ screens/HomeScreen.jsx
@@ -1,12 +1,12 @@
-import { Button } from "react-native-paper";
-import { Text, View } from "react-native";
+
+import { Button, StyleSheet, View } from "react-native";
 import MyHeader from "../components/MyHeader";
 
 export default function HomeScreen({navigation}) {
     return(
         <View>
             <MyHeader title={'Home'}/>
-            <Button onPress={() => navigation.navigate("BatteryInfo")}>Mostrar Informações da Bateria</Button>
+            <Button title="MOSTRAR BATERIA" onPress={() => navigation.navigate("BatteryInfo")}/>
         </View>
     )
 }
